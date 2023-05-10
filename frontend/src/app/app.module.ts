2097d375cc7e594dcab860e1d177b6c123f2346d
ivasquez23 <130954151+ivasquez23@users.noreply.github.com>
2023-05-09 22:59:57 -0600

    "Se cargo modulos en angular, carpetas y servicio"

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//Angular material
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+import { AppComponent } from './app.component';
+import { ListPersonasComponent } from './components/list-personas/list-personas.component';
+import { AgregarEditarPersonaComponent } from './components/agregar-editar-persona/agregar-editar-persona.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListPersonasComponent,
+    AgregarEditarPersonaComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
